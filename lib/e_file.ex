98f913926d2b37d4bbdc2371be7df91edcac0b3b
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2018-12-06 14:57:05 +0900

    add enumerable file

--- lib/e_file.ex
+++ lib/e_file.ex
@@ -1,18 +1,39 @@
 defmodule EFile do
-  @moduledoc """
-  Documentation for EFile.
-  """
+  defstruct [:filename, :file, opening: false]
 
-  @doc """
-  Hello world.
+  def open(filename) do
+    case File.open(filename) do
+      {:ok, device} ->
+        {:ok, %EFile{file: device, filename: filename, opening: true}}
 
-  ## Examples
+      {:error, _} = err ->
+        err
+    end
+  end
 
-      iex> EFile.hello()
-      :world
+  def close(%EFile{file: file} = efile) do
+    File.close(file)
+    %{efile | file: nil, opening: false}
+  end
 
-  """
-  def hello do
-    :world
+  def readline(%EFile{file: file}) do
+    IO.read(file, :line)
+  end
+
+  defimpl Enumerable do
+    def count(%EFile{}), do: {:error, __MODULE__}
+    def member?(%EFile{}, _), do: {:error, __MODULE__}
+    def slice(%EFile{}), do: {:error, __MODULE__}
+    def reduce(%EFile{}, {:halt, acc}, _), do: {:halted, acc}
+    def reduce(%EFile{} = efile, {:suspend, acc}, fun), do: {:suspended, acc, &reduce(efile, &1, fun)}
+    def reduce(%EFile{} = efile, {:cont, acc}, fun) do
+      case EFile.readline(efile) do
+        :eof ->
+          {:done, acc}
+
+        line ->
+          reduce(efile, fun.(String.trim_trailing(line, "\n"), acc), fun)
+      end
+    end
   end
 end
--- test/e_file_test.exs
+++ test/e_file_test.exs
@@ -1,8 +1,4 @@
 defmodule EFileTest do
   use ExUnit.Case
   doctest EFile
-
-  test "greets the world" do
-    assert EFile.hello() == :world
-  end
 end
